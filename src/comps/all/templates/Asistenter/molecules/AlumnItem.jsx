bd141c1363d6a560397a71146514fb8bbe33168b
ElZeke0987 <eliassebastiansegatori@gmail.com>
2024-10-10 13:33:18 -0300

    Old selects commented

--- src/comps/all/templates/Asistenter/molecules/AlumnItem.jsx
+++ src/comps/all/templates/Asistenter/molecules/AlumnItem.jsx
@@ -3,7 +3,7 @@ export default function AlumnItem({almn}){
     return(
         <div className="alumn-item" id="${almn.id}">
             <label>Presente:<input className="pres-alumn" type="checkbox" checked={true}/></label><br/>
-            Alumno:<span className="name">${almn.nombre} {almn.apellido}</span>
+            Alumno:<span className="name">${almn.nombre}  {almn.apellido}</span>
             <div>Grupo de taller: <span className="taller-grupo">{almn.grupo_tal}</span></div>
             <div>DNI: {almn.dni}</div>
             <div>Inasistencias: ${almn.inasistencias}</div>
