b411c1143357041f99252ffba0343316434a6157
ElZeke0987 <eliassebastiansegatori@gmail.com>
2024-10-15 11:19:41 -0300

    Alumn item handler of presence and just asist 100%

--- src/comps/all/templates/Asistenter/molecules/AlumnItem.jsx
+++ src/comps/all/templates/Asistenter/molecules/AlumnItem.jsx
@@ -1,7 +1,69 @@
 import { handleAlumnClick, handleStyleCheckTourn } from "../../../../styling";
+import { useState } from "react";
 
 export default function AlumnItem({almn}){
-    console.log("rendering: ", almn.nombre);
+    const [presenceChecked, setPresenceChecked] = useState(true);
+    const [justifiedChecked, setJustifiedChecked] = useState(false);
+
+    const handleStyleCheckTourn = (e) => {
+        // Si se hace click sobre el checkbox de la presencia
+        if (e.target.parentElement.classList.contains('presence-cont')) {
+        setPresenceChecked(!presenceChecked);
+        }
+        // Si se hace click sobre el checkbox de falta justificada
+        if (e.target.parentElement.classList.contains('justas-cont')) {
+        setJustifiedChecked(!justifiedChecked);
+        }
+    };
+
+  const handleAlumnClick = (e) => {
+    // Solo click para alternar el checkbox de presence-cont
+    if(e.target.parentElement.classList.contains('presence-cont')||e.target.classList.contains('justas-cont')) {
+        console.log("e.target  ", e.target)
+        
+    }else{
+        if (e.detail === 1) {
+            setPresenceChecked(!presenceChecked);
+            setJustifiedChecked(false);
+        }
+        // Doble click para alternar el checkbox de justas-cont
+        if (e.type === "dblclick") {
+            setPresenceChecked(false);
+            setJustifiedChecked(!justifiedChecked);
+        }
+    }
+  };
+
+  return (
+    <div className="alumn-item" id={almn.id} onDoubleClick={handleAlumnClick} onClick={handleAlumnClick}>
+      <div className="pres-check presence-cont" onClick={handleStyleCheckTourn}>
+        <label>Presente:</label>
+        <input
+          className="pres-alumn"
+          type="checkbox"
+          checked={presenceChecked}
+          onChange={() => setPresenceChecked(!presenceChecked)}
+        />
+      </div>
+
+      Alumno:<span className="name">{almn.nombre} {almn.apellido}</span>
+      <div>Grupo de taller: <span className="taller-grupo">{almn.grupo_tal}</span></div>
+      <div>DNI: {almn.dni}</div>
+      <div>Inasistencias: {almn.inasistencias}</div>
+
+      <div className="pres-check justas-cont" onClick={handleStyleCheckTourn}>
+        <label>Falta justificada:</label>
+        <input
+          className="just-asist-alumn"
+          type="checkbox"
+          checked={justifiedChecked}
+          onChange={() => setJustifiedChecked(!justifiedChecked)}
+        />
+      </div>
+    </div>
+  );
+}
+    /*let [presen]
     return(
         <div className="alumn-item" id={almn.id} onDoubleClick={handleAlumnClick} onClick={handleAlumnClick}>
             <div className="pres-check presence-cont" onClick={handleStyleCheckTourn}>
@@ -19,4 +81,4 @@ export default function AlumnItem({almn}){
             </div>
         </div>
     )
-}
+}*/
--- src/comps/styling.js
+++ src/comps/styling.js
@@ -19,21 +19,43 @@ export function handleStyleCheckTourn(e){
     elem.classList.toggle("checked-opt");
 }
 
+function setAllOthers(e){
+    let allOthers = e.currentTarget.querySelectorAll("input");
+    allOthers.forEach(i=>i.checked=false);
+}
+function setChecked(clase, elem){
+    let inp = elem.querySelector(clase);
+    inp.checked = inp.checked?false:true;
+    inp.parentElement.classList.toggle("alumn-checked");
+
+}
 export function handleAlumnClick(e){
-    
-    if(!e.target.classList.contains("just-asist-alumn")||!e.target.classList.contains("pres-alumn")){
-        let elem= e.currentTarget;
-        if(e.type=="dblclick"){
-            
-            let inp = elem.querySelector(".just-asist-alumn");
-            inp.checked = inp.checked?false:true;
-            inp.parentElement.classList.toggle("alumn-checked");
-        }
+    /*
+        Un click: presencia = true;(1)
+        Doble click: falta justificada = true;(2)
+        click sobre los mismos input basandose en la clase del mismo: input = true;(3)
+        All others turn to false beforee turn the other one inp to true (4)
+    */
+    let elem= e.currentTarget;
+   console.log("e.currentTarget: ", elem, " e.target: ", e.target);
+   if(e.target.classList.contains("justas-cont")||e.target.classList.contains("presence-cont")||
+   e.target.parentElement.classList.contains("justas-cont")||e.target.parentElement.classList.contains("presence-cont")){
+    setAllOthers(e);
+    setChecked("input", e.target);
+    return
+   }//(3) verificacion
+
+    if(e.type=="dblclick"){//(2)
+        setAllOthers(e);
         
-            let inp = elem.querySelector(".pres-alumn");
-            inp.checked = inp.checked?false:true;
-            inp.parentElement.classList.toggle("alumn-checked");
+        setChecked(".just-asist-alumn", elem);
+        return
     }
-        
+    //(1)
+    setAllOthers(e);
+    setChecked(".pres-alumn", elem);
+    return
+
+    
 }
 
